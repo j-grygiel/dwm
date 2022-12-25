b32af34743f2edfc36fc17228c7f938400ecc9e8
j-grygiel <jakubgrygiel@protonmail.com>
2022-12-25 20:14:03 +1000

    add swallowfloating var

--- config.h
+++ config.h
@@ -7,6 +7,7 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 /* static const unsigned int gappx     = 5;        /1* gaps size between windows *1/ */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
+static const int swallowfloating	= 0;
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel		= 0;
 static const char *fonts[]          = { "Iosevka Custom:size=10" };
