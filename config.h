4a726c688ba7e4309cb0d721bd41ae28dc832795
j-grygiel <jakubgrygiel@protonmail.com>
2022-12-26 15:18:19 +1000

    change colors

--- config.h
+++ config.h
@@ -16,15 +16,15 @@ static const char col_gray1[]       = "#282828";
 // inactive window border colour
 static const char col_gray2[]       = "#504935";
 // font colour
-static const char col_gray3[]       = "#d5c4a1";
+static const char col_white1[]       = "#d5c4a1";
 // current tag and current window font colour
 static const char col_gray4[]       = "#282828";
 // bar second colour (blue) and active window border colour
 static const char col_green[]        = "#b8bb26";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_green,  col_green  },
+	[SchemeNorm] = { col_white1, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_green, col_gray1,  col_green  },
 };
 
 /* tagging */
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_white1, "-sb", col_green, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
