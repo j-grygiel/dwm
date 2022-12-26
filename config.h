d702c1e5cc02545f71afe31a0979618202391ec1
j-grygiel <jakubgrygiel@protonmail.com>
2022-12-26 12:21:01 +1000

    increase bar font size

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel		= 0;
-static const char *fonts[]          = { "Iosevka Custom:size=10" };
-static const char dmenufont[]       = "Iosevka Custom:size=10";
+static const char *fonts[]          = { "Iosevka Custom:size=12", "Hack Nerd Font Mono:size=14" };
+static const char dmenufont[]       = "Iosevka Custom:size=12";
 // background colour
 static const char col_gray1[]       = "#282828";
 // inactive window border colour
