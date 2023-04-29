38182f3d250f19c27efbc72e5e6b2fa558ca1548
j-grygiel <jakubgrygiel@protonmail.com>
2023-04-29 12:07:18 +1000

    add xkill keybind

--- config.h
+++ config.h
@@ -101,6 +101,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,             			XK_c,      killclient,     {0} },
+	{ MODKEY,						XK_x,	   spawn,          SHCMD("xkill") },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
